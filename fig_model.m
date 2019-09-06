c5590d1c3d5b45a93481d7378cb91b73f2ccc2c2
Srinivas Gorur-Shandilya <code@srinivas.gs>
2019-09-05 21:18:30 -0400

    added some annotations to the figure with the model

--- fig_model.m
+++ fig_model.m
@@ -66,6 +66,7 @@ Model.evaluate;
 
 time = linspace(0,3,300);
 plot(ax(2).hero,time,Model.Prediction,'r')
+title(ax(2).hero,alldata(2).odour_name,'FontWeight','normal')
 
 
 % now show 4 more examples in a smaller plot
@@ -100,7 +101,7 @@ Model.Parameters = p(14,idx);
 Model.evaluate;
 
 plot(ax(3).hero,time,Model.Prediction,'r')
-
+title(ax(3).hero,alldata(14).odour_name,'FontWeight','normal')
 
 show_these = [25  15 16 18];
 
@@ -142,6 +143,8 @@ Model.evaluate;
 
 plot(ax(4).hero,time,Model.Prediction,'r')
 
+title(ax(4).hero,alldata(6).odour_name,'FontWeight','normal')
+
 
 show_these = [17  23 19 20];
 
@@ -180,6 +183,8 @@ Model.evaluate;
 
 plot(ax(5).hero,time,Model.Prediction,'r')
 
+title(ax(5).hero,alldata(8).odour_name,'FontWeight','normal')
+
 
 show_these = [5 1 12 27];
 
@@ -217,19 +222,49 @@ axlib.move(ax(2).examples,'up',.03)
 axlib.move(ax(3).examples,'up',.02)
 
 
+% add some annotations
+a(1) = annotation('textarrow',[0.1552    0.1958],[0.7491    0.7060],'String','Sharp rise');
+a(2) = annotation('textarrow',[0.35    0.3],[.7 .7],'String','Sharp drop');
+a(3) = annotation('textarrow',[0.4    0.4],[.52 .48],'String','Lingering plateau');
+a(4) = annotation('textarrow',[0.1552    0.1958],[0.54    0.5],'String','Sharp rise');
+a(5) = annotation('textarrow',[0.4    0.4],[.31 .34],'String','Large plateau');
+a(6) = annotation('textarrow',[0.35    0.3],[.37 .37],'String','Sharp drop');
+a(7) = annotation('textarrow',[0.16    0.21],[0.36    0.33],'String','Slow rise');
+a(8) = annotation('textarrow',[0.16    0.21],[0.2    0.17],'String','Slow rise');
+a(9) = annotation('textarrow',[0.35    0.3],[.2 .18],'String','Slower decay');
 
 
 % now make a supplementary figure showing the range of parameters
-figure('outerposition',[300 300 880 901],'PaperUnits','points','PaperSize',[880 901]); hold on
+figure('outerposition',[1 1 880 901],'PaperUnits','points','PaperSize',[880 901]); hold on
+
+clear ax
 
 fn = {'w','tau_a','k_d'};
 
 
-% don't show zero or 1 parameter fits
-show_these = setdiff(1:27,[2 3 4 7 26 22]);
+
+
+% don't show zero or 1 parameter fits, and show them in the order that they are presented in 
+
+show_these = [14 25  15 16 18 6 17  23 19 20 8 5 1 12 27];
+
+C = zeros(15,3);
+c = lines;
+
+for i = 1:5
+	C(i,:) = c(1,:);
+end
+for i = 6:10
+	C(i,:) = c(2,:);
+end
+for i = 11:15
+	C(i,:) = c(4,:);
+end
+
+
 
 for i = 1:length(fn)
-	subplot(3,1,i); hold on
+	ax(i) = subplot(3,1,i); hold on
 
 	X = {};
 
@@ -238,15 +273,15 @@ for i = 1:length(fn)
 
 		plot_this = [p(show_these(j),:).(fn{i})];
 
-		x = 0*plot_this(:) + j + randn(length(plot_this),1)/10;
-		plot(x,plot_this,'k.')
+		errorbar(j,mean(plot_this),std(plot_this),'Color',C(j,:))
+		plot(j,mean(plot_this),'o','Color',C(j,:))
 
 		X{j} = alldata(show_these(j)).odour_name;
 		X{j} = regexprep(X{j},'[\n\r]+','');
 
 	end
 
-	set(gca,'YScale','log','XLim',[0 length(show_these)+1],'XTickLabel','')
+	set(gca,'YScale','log','XLim',[0 length(show_these)+1],'XTickLabel','','YMinorGrid','on')
 	ylabel(fn{i})
 
 
@@ -256,4 +291,13 @@ end
 set(gca,'XTickLabel',X,'XTick',1:length(show_these),'XTickLabelRotation',45)
 
 figlib.pretty()
+
+for i = 1:length(ax)
+	ax(i).Position(4) = .2;
+end
+
+ax(3).Position(2) = .2;
+ax(2).Position(2) = .45;
+ax(1).Position(2) = .7;
+ax(3).YLim = [10 1e3];
 
